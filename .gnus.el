89ea32068c5afa3cc000c7c98b5b51df184c7365
rails <rails@fidel.cns.montana.edu>
2009-05-20 13:13:53 -0600

    Altered to make the null gnus nntp+news message go away.

--- .gnus.el
+++ .gnus.el
@@ -3,35 +3,57 @@
 (load-library "smtpmail")
 (load-library "nnimap")
 (load-library "starttls")
-(require 'nnir)
-
+(load-library "nnir")
 
 (setq gnus-select-method '(nnimap "imap.gmail.com"
            (nnimap-address "imap.gmail.com")
-           (nnimap-server-port 993)
-           (nnimap-authinfo-file "~/.authinfo")
-	   (nnir-search-engine imap)
-           (nnimap-stream ssl)))
+					            (nnimap-server-port 993)
+											           (nnimap-authinfo-file "~/.authinfo")
+																      (nnir-search-engine imap)
+																			           (nnimap-stream ssl)))
+
+
+
+(setq gnus-secondary-select-methods
+      '(
+
+      (nnimap "finn.cns.montana.edu"
+	      (nnimap-list-pattern ("~/mail/in-SPAM_BUCKET"))
+		(nnimap-address "finn.cns.montana.edu")
+		(nnimap-server-port 993)
+		(nnir-search-engine imap)
+		(nnimap-authinfo-file "~/.authinfo")
+		(nnimap-stream ssl)
+		)
+      )
+)
 
 (setq smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
-      smtpmail-smtp-server "smtp.gmail.com"
-      smtpmail-default-smtp-server "smtp.gmail.com"
-      send-mail-function 'smtpmail-send-it
-      message-send-mail-function 'smtpmail-send-it
-      smtpmail-smtp-service 587
-      ;; smtpmail-auth-credentials '(("smtp.gmail.com"
-      ;;  587
-      ;;  "livingood.pw@gmail.com"
-      ;;  nil))
+smtpmail-smtp-server "smtp.gmail.com"
+smtpmail-default-smtp-server "smtp.gmail.com"
+send-mail-function 'smtpmail-send-it
+message-send-mail-function 'smtpmail-send-it
+smtpmail-smtp-service 587
+;; smtpmail-auth-credentials '(("smtp.gmail.com"
+;; 587
+;; "livingood.pw@gmail.com"
+;; nil))
 )
 (add-hook 'gnus-topic-mode-hook 'gnus-topic-mode)
 
-(require 'bbdb)
-(bbdb-initialize)
-(add-hook 'gnus-startup-hook 'bbdb-insinuate-gnus)
-(add-hook 'gnus-startup-hook 'bbdb-insinuate-message)
-
-
-;; (setq gnus-invalid-group-regexp "[:`'\"]\\|^$")
-(setq gnus-ignored-newsgroups  "")
+(setq gnus-invalid-group-regexp "[:`'\"]\\|^$")
+(setq gnus-ignored-newsgroups "")
 (setq gnus-outgoing-message-group "[Google Mail]/Sent Mail")
+
+     (setq gnus-extract-address-components
+           'mail-extract-address-components)
+
+(setq mail-yank-prefix "> ")
+
+;; from http://emacs.wordpress.com/category/gnus/
+
+;; (setq gnus-summary-line-format "%U%R%z%a%I%(%[%d:%ub%-23,23f%]%) %s\n")
+
+;;C-h v gnus-summary-line-format
+
+(setq gnus-summary-mark-below 0)
